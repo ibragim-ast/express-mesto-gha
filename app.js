3da0f3e67537e1a561f02c195ad1bf26097664d9
ibragim-ast <ibragim.ast@gmail.com>
2023-07-11 11:22:26 +0300

    changes in app

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 const express = require('express');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-const { PORT, DB_URI } = require('./config');
 
 const app = express();
 
-const catchErrorsMiddleware = require('./middlewares/catchErrors');
+const { PORT = 3000 } = process.env;
 
-app.use(express.json());
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
-mongoose.connect(DB_URI, {});
+app.use(bodyParser.json());
 
-app.use('/', require('./routes/index'));
+const { ERROR_CODE_NOT_FOUND } = require('./utils/constants');
 
-app.use(catchErrorsMiddleware);
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64998afeb03e1c77a35f40aa',
+  };
 
-app.listen(PORT, () => { });
+  next();
+});
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Слишком много запросов с вашего IP, попробуйте позже',
+});
+
+const cardsRouter = require('./routes/cards');
+const usersRouter = require('./routes/users');
+
+app.use(helmet());
+app.use(limiter);
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
+app.use('/*', (req, res) => {
+  res.status(ERROR_CODE_NOT_FOUND).send({ message: 'Запись не найдена!' });
+});
+
+app.listen(PORT, () => {
+  console.log('Сервер запущен');
+});
